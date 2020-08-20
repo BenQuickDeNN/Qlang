f165e4796378da93865125bc5bc9fadbb44f52f6
qingbei <qingbei@pinduoduo.com>
2020-08-20 15:03:00 +0800

    fix file.hpp

--- include/file/file.hpp
+++ include/file/file.hpp
@@ -2,14 +2,23 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iostream>
 static std::string readStrFromFile(const std::string &filename)
 {
     using namespace std;
     ifstream ifile(filename);
-    ostringstream buf;
-    char c;
-    while (buf && ifile.get(c))
-        buf.put(c);
-    ifile.close();
-    return buf.str();
+    if (ifile)
+    {
+        ostringstream buf;
+        char c;
+        while (buf && ifile.get(c))
+            buf.put(c);
+        ifile.close();
+        return buf.str();
+    }
+    else
+    {
+        cerr << "cannot open the file \"" << filename << "\"" << endl;
+        return "";
+    }
 }
